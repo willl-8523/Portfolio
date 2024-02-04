33fbb78765b81abb31269cb0e6ab86344561849a
willl85 <ngowilfried@gmail.com>
2024-02-04 20:12:33 +0100

    Update Formations page and data

--- src/components/formations/Formations.component.jsx
+++ src/components/formations/Formations.component.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import CV from '../../assets/CV_Mbenock_Wilfried.pdf';
+import Graduation from '../../assets/logo_competences/graduation-cap.png';
+import formationsData from '../../data/formationsData';
 import TimelineContent from './TimelineContent.component';
 import './formation.style.css';
-import formationsData from '../../data/formationsData';
-import Graduation from '../../assets/logo_competences/graduation-cap.png';
 
 const FormationsComponent = () => {
   return (
-    <div className="my-6 px-5 max-w-6xl w-full">
+    <div className="my-6 px-5 max-w-6xl w-full mx-auto">
       <div className="flex flex-wrap flex-row items-center min-h-screen">
         <div className="formationHeader max-w-full basis-full flex-flex-xs-12 px-2 py-5 text-center text-very-dark-gray">
           <h1 className="text-32px font-black uppercase tracking-widest my-2.5 relative">
@@ -21,12 +20,11 @@ const FormationsComponent = () => {
           </h1>
           <p className="leading-7 tracking-wider font-light text-sm">
             Below is a timeline of the trainings and professional certifications
-            I have completed. For more details, you can refer to my{' '}
-            <Link to={CV} target="_blank" className="text-link hover:underline">
-              CV
-            </Link>{' '}
-            or{' '}
-            <Link to={'/contact'} className="text-link hover:underline">
+            I have completed. For more details, you can{' '}
+            <Link
+              to={'mailto:ngowilfried@gmail.com'}
+              className="text-link hover:underline"
+            >
               contact
             </Link>{' '}
             me.
