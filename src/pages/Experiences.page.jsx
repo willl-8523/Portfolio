94fe916a1c5e4aecc4f7b471ba408bbf3ad0ed1f
willl85 <ngowilfried@gmail.com>
2024-02-04 20:05:57 +0100

    Delete Experiences page

--- src/pages/Experiences.page.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ExperiencesComponent from '../components/experiences/Experiences.component';
-
-const Experiences = () => {
-  return <ExperiencesComponent />;
-};
-
-export default Experiences;
